156a5e191413d15ce22295d9a3ad0996c4b8da6a
piusgori <piusgori@gmail.com>
2024-08-28 12:11:55 +0300

    Switch

--- control_structures/bank.go
+++ control_structures/bank.go
@@ -18,10 +18,12 @@ func main() {
 		var choice int
 		fmt.Print("Your choice: ")
 		fmt.Scan(&choice)
-	
-		if choice == 1 {
+
+		switch choice {
+		case 1:
 			fmt.Println("Your account balance is", accountBalance)
-		} else if choice == 2 {
+
+		case 2:
 			fmt.Print("Your deposit: ")
 			var depositAmount float64
 			fmt.Scan(&depositAmount)
@@ -33,7 +35,8 @@ func main() {
 	
 			accountBalance += depositAmount
 			fmt.Println("Balance updated! New amount:", accountBalance)
-		} else if choice == 3{
+
+		case 3:
 			fmt.Print("Withdrawal amount: ")
 			var withdrawalAmount float64
 			fmt.Scan(&withdrawalAmount)
@@ -50,11 +53,49 @@ func main() {
 	
 			accountBalance -= withdrawalAmount
 			fmt.Println("Balance updated! New amount:", accountBalance)
-		} else {
+
+		default:
 			fmt.Println("Goodbye!")
-			// return
-			break
+			fmt.Println("Thanks for choosing our bank")
+			return
 		}
+	
+		// if choice == 1 {
+		// 	fmt.Println("Your account balance is", accountBalance)
+		// } else if choice == 2 {
+		// 	fmt.Print("Your deposit: ")
+		// 	var depositAmount float64
+		// 	fmt.Scan(&depositAmount)
+	
+		// 	if depositAmount <= 0 {
+		// 		fmt.Println("Invalid amount. Must be greater than 0.")
+		// 		continue
+		// 	}
+	
+		// 	accountBalance += depositAmount
+		// 	fmt.Println("Balance updated! New amount:", accountBalance)
+		// } else if choice == 3{
+		// 	fmt.Print("Withdrawal amount: ")
+		// 	var withdrawalAmount float64
+		// 	fmt.Scan(&withdrawalAmount)
+	
+		// 	if withdrawalAmount <= 0 {
+		// 		fmt.Println("Invalid amount. Must be greater than 0.")
+		// 		continue
+		// 	}
+	
+		// 	if withdrawalAmount > accountBalance {
+		// 		fmt.Println("Invalid amount. You can't withdraw more than you have.")
+		// 		continue
+		// 	}
+	
+		// 	accountBalance -= withdrawalAmount
+		// 	fmt.Println("Balance updated! New amount:", accountBalance)
+		// } else {
+		// 	fmt.Println("Goodbye!")
+		// 	// return
+		// 	break
+		// }
 	}
 
 	fmt.Println("Thanks for choosing our bank")
