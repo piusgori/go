eb43ef8f7bde16b864f54d47642f12dd0f92a7ca
piusgori <piusgori@gmail.com>
2024-08-28 12:56:13 +0300

    Split code across files

--- control_structures/bank.go
+++ control_structures/bank.go
@@ -31,22 +31,19 @@ func main() {
 
 	accountBalance, err := getBalanceFromFile();
 
+	
 	if err != nil {
 		fmt.Println("Error")
 		fmt.Println(err)
 		fmt.Println("----------")
 		panic("Can't continue, sorry.")
 	}
-
-	fmt.Println("Welcome to Go Bank!")
-
-	for {
-		fmt.Println("What do you want to do?")
-		fmt.Println("1. Check balance")
-		fmt.Println("2. Deposit money")
-		fmt.Println("3. Withdraw money")
-		fmt.Println("4. Exit")
 	
+	fmt.Println("Welcome to Go Bank!")
+	
+	for {
+		presentOptions()
+		
 		var choice int
 		fmt.Print("Your choice: ")
 		fmt.Scan(&choice)
@@ -93,44 +90,5 @@ func main() {
 			fmt.Println("Thanks for choosing our bank")
 			return
 		}
-	
-		// if choice == 1 {
-		// 	fmt.Println("Your account balance is", accountBalance)
-		// } else if choice == 2 {
-		// 	fmt.Print("Your deposit: ")
-		// 	var depositAmount float64
-		// 	fmt.Scan(&depositAmount)
-	
-		// 	if depositAmount <= 0 {
-		// 		fmt.Println("Invalid amount. Must be greater than 0.")
-		// 		continue
-		// 	}
-	
-		// 	accountBalance += depositAmount
-		// 	fmt.Println("Balance updated! New amount:", accountBalance)
-		// } else if choice == 3{
-		// 	fmt.Print("Withdrawal amount: ")
-		// 	var withdrawalAmount float64
-		// 	fmt.Scan(&withdrawalAmount)
-	
-		// 	if withdrawalAmount <= 0 {
-		// 		fmt.Println("Invalid amount. Must be greater than 0.")
-		// 		continue
-		// 	}
-	
-		// 	if withdrawalAmount > accountBalance {
-		// 		fmt.Println("Invalid amount. You can't withdraw more than you have.")
-		// 		continue
-		// 	}
-	
-		// 	accountBalance -= withdrawalAmount
-		// 	fmt.Println("Balance updated! New amount:", accountBalance)
-		// } else {
-		// 	fmt.Println("Goodbye!")
-		// 	// return
-		// 	break
-		// }
 	}
-
-	// fmt.Println("Thanks for choosing our bank")
 }
