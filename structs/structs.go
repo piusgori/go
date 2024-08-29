73e470ab199b6b1d3faacb9671cef3892e0a52eb
piusgori <piusgori@gmail.com>
2024-08-29 20:11:10 +0300

    Note app

--- structs/structs.go
+++ structs/structs.go
@@ -1,58 +1,31 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"time"
+	"example.com/structs/user";
 )
-
-type User struct {
-	firstName string
-	lastName string
-	birthdate string
-	createdAt time.Time
-}
-
-func (u *User) outputUserDetails () {
-	fmt.Println(u.firstName, u.lastName, u.birthdate)
-}
-
-func (u *User) clearUserName () {
-	u.firstName = ""
-	u.lastName = ""
-}
-
-func newUser(firstName, lastName, birthdate string) (*User, error) {
-
-	if firstName == "" || lastName == "" || birthdate == "" {
-		return nil, errors.New("first name, last name and birth date are required")
-	}
-
-	return &User{
-		firstName: firstName,
-		lastName: lastName,
-		birthdate: birthdate,
-		createdAt: time.Now(),
-	}, nil
-}
 
 func main() {
 	userFirstName := getUserData("Please enter your first name: ")
 	userLastName := getUserData("Please enter your last name: ")
 	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
-	appUser, err := newUser(userFirstName, userLastName, userBirthdate)
+	appUser, err := user.NewUser(userFirstName, userLastName, userBirthdate)
+
+	appUser.OutputUserDetails()
 
 	if (err != nil) {
 		fmt.Println(err)
 		return
 	}
 
+	admin := user.NewAdmin("test@test.com", "password")
+
 	// ... do something awesome with that gathered data!
 
-	appUser.outputUserDetails()
-	appUser.clearUserName()
-	appUser.outputUserDetails()
+	admin.OutputUserDetails()
+	admin.ClearUserName()
+	admin.OutputUserDetails()
 }
 
 func getUserData(promptText string) string {
