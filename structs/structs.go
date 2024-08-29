9eadcfb02826084e17f8d84b59dfe3f4a38da843
piusgori <piusgori@gmail.com>
2024-08-29 18:52:09 +0300

    Constructor funcs for validation

--- structs/structs.go
+++ structs/structs.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"time"
 )
@@ -12,8 +13,27 @@ type User struct {
 	createdAt time.Time
 }
 
-func (u User) outputUserDetails () {
+func (u *User) outputUserDetails () {
 	fmt.Println(u.firstName, u.lastName, u.birthdate)
+}
+
+func (u *User) clearUserName () {
+	u.firstName = ""
+	u.lastName = ""
+}
+
+func newUser(firstName, lastName, birthdate string) (*User, error) {
+
+	if firstName == "" || lastName == "" || birthdate == "" {
+		return nil, errors.New("first name, last name and birth date are required")
+	}
+
+	return &User{
+		firstName: firstName,
+		lastName: lastName,
+		birthdate: birthdate,
+		createdAt: time.Now(),
+	}, nil
 }
 
 func main() {
@@ -21,21 +41,23 @@ func main() {
 	userLastName := getUserData("Please enter your last name: ")
 	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
-	var appUser User = User{
-		firstName: userFirstName,
-		lastName: userLastName,
-		birthdate: userBirthdate,
-		createdAt: time.Now(),
+	appUser, err := newUser(userFirstName, userLastName, userBirthdate)
+
+	if (err != nil) {
+		fmt.Println(err)
+		return
 	}
 
 	// ... do something awesome with that gathered data!
 
-	User.outputUserDetails(appUser)
+	appUser.outputUserDetails()
+	appUser.clearUserName()
+	appUser.outputUserDetails()
 }
 
 func getUserData(promptText string) string {
 	fmt.Print(promptText)
 	var value string
-	fmt.Scan(&value)
+	fmt.Scanln(&value)
 	return value
 }
