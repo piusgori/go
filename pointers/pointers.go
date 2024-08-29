8f94acaf862946cfae81c981c8c7f1ed8f9d63b6
piusgori <piusgori@gmail.com>
2024-08-29 15:33:49 +0300

    Updating values using pointers

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -8,10 +8,12 @@ func main() {
 	var agePointer *int = &age
 
 	fmt.Println("Age:", *agePointer)
-	adultYears := getAdultYears(agePointer)
-	fmt.Println(adultYears)
+	editAgeToAdultYears(agePointer)
+
+	fmt.Println(age)
 }
 
-func getAdultYears(age *int) int {
-	return *age - 18
+func editAgeToAdultYears(age *int) {
+	*age = *age - 18
+	// return *age - 18
 }
