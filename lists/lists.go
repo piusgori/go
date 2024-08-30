2a2eb8f7f19fcc3a19a967ccdd5989c13ac74567
piusgori <piusgori@gmail.com>
2024-08-30 20:05:06 +0300

    Slicing

--- lists/lists.go
+++ lists/lists.go
@@ -13,4 +13,7 @@ func main() {
 	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
 	fmt.Println(prices)
 	fmt.Println(productNames[0])
+
+	featuredPrices := prices[1:3]
+	fmt.Println(featuredPrices)
 }
