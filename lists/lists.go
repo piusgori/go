165ec9e39687ae8c0cb7941e31e78a5eea6d41e9
piusgori <piusgori@gmail.com>
2024-08-31 10:51:13 +0300

    Practice

--- lists/lists.go
+++ lists/lists.go
@@ -9,11 +9,16 @@ type Product struct {
 }
 
 func main() {
-	var productNames [4]string = [4]string{"A Book"}
-	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
-	fmt.Println(prices)
-	fmt.Println(productNames[0])
 
-	featuredPrices := prices[1:3]
-	fmt.Println(featuredPrices)
+	prices := []float64{10.99, 8.99}
+	updatedSlice := append(prices, 5.99)
+	fmt.Println(updatedSlice)
+
+	// var productNames [4]string = [4]string{"A Book"}
+	// prices := [4]float64{10.99, 9.99, 45.99, 20.0}
+	// fmt.Println(prices)
+	// fmt.Println(productNames[0])
+
+	// featuredPrices := prices[1:3]
+	// fmt.Println(len(featuredPrices), cap(featuredPrices))
 }
