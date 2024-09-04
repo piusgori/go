348f3cdf90f0d24aafaa6d21af36fff8dffcfc8d
piusgori <piusgori@gmail.com>
2024-09-04 13:40:06 +0300

    Make changes

--- functions_as_values/main.go
+++ functions_as_values/main.go
@@ -8,12 +8,21 @@ type transformFn func(int) int
 
 func main() {
 	numbers := []int{1,2,3,4}
+	moreNumbers := []int{5, 1, 2}
 
 	doubled := transformNumbers(&numbers, double);
-
 	trippled := transformNumbers(&numbers, tripple);
 
 	fmt.Println(doubled, trippled)
+
+	transformerFn1 := getTransformerFunction(&numbers)
+	transformerFn2 := getTransformerFunction(&moreNumbers)
+
+
+	transformedNumbers := transformNumbers(&numbers, transformerFn1)
+	moreTransformedNumbers := transformNumbers(&moreNumbers, transformerFn2)
+
+	fmt.Println(transformedNumbers, moreTransformedNumbers)
 }
 
 func transformNumbers(numbers *[]int, transform transformFn) []int {
@@ -23,6 +32,14 @@ func transformNumbers(numbers *[]int, transform transformFn) []int {
 	}
 
 	return dNumbers
+}
+
+func getTransformerFunction (numbers *[]int) transformFn {
+	if (*numbers)[0] == 1 {
+		return double
+	} else {
+		return tripple
+	}
 }
 
 func double(number int) int {
