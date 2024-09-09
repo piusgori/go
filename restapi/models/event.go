ef325ca7a7230f702bb49742248e7c852e9247da
piusgori <piusgori@gmail.com>
2024-09-09 13:32:36 +0300

    Rest API

---
+++ restapi/models/event.go
@@ -0,0 +1,23 @@
+package models
+
+import "time"
+
+type Event struct {
+	ID          int
+	Name        string`binding:"required"`
+	Description string`binding:"required"`
+	Location    string`binding:"required"`
+	Date    time.Time`binding:"required"`
+	UserID      int
+}
+
+var events = []Event{}
+
+func (e Event) Save () {
+	// save the event to the database
+	events = append(events, e)
+}
+
+func GetAllEvents () []Event {
+	return events
+}
